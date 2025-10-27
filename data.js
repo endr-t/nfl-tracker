c1a3fe82a25ef56c6aa09cb69ef931cf1e9810e1
endr-t <tyronemckenzie30@gmail.com>
2025-10-26 20:48:41 -0600

    2nd page added

--- data.js
+++ data.js
@@ -20,17 +20,38 @@ async function display() {
 
   for (let i = 0; i <= 15; i++) {
     element = "img" + i;
-    document.getElementById(element).src = result_img[i];
+
+    if (document.getElementById(element) !== null) {
+      document.getElementById(element).src = result_img[i];
+    }
   }
 
   for (let a = 0; a <= 7; a++) {
     element_date = "date" + a;
-    document.getElementById(element_date).innerHTML = result_date[a];
+    if (document.getElementById(element_date) !== null) {
+      document.getElementById(element_date).innerHTML = result_date[a];
+    }
   }
 
   for (let a = 0; a <= 15; a++) {
     element_team = "team" + a;
-    document.getElementById(element_team).innerHTML = result_team[a];
+    if (document.getElementById(element_team) !== null) {
+      document.getElementById(element_team).innerHTML = result_team[a];
+    }
+  }
+
+  for (let i = 16; i <= 19; i++) {
+    element = "img" + i;
+    if (document.getElementById(element) !== null) {
+      document.getElementById(element).src = result_img[i];
+    }
+  }
+
+  for (let a = 8; a <= 9; a++) {
+    element_date = "date" + a;
+    if (document.getElementById(element_date) !== null) {
+      document.getElementById(element_date).innerHTML = result_date[a];
+    }
   }
 }
 
